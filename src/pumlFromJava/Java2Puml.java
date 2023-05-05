6f9a750ed472fd65c6b94812c6441641ab145af9
xeragon <wladimir.bozenko@etu.unistra.fr>
2023-05-05 10:13:04 +0200

    lesgo on a lancer javadoc

--- src/pumlFromJava/Java2Puml.java
+++ src/pumlFromJava/Java2Puml.java
@@ -1,12 +1,14 @@
 package pumlFromJava;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.spi.ToolProvider;
 
 public class Java2Puml
 {
-
     public static void main(String[] args)
     {
+        String[] argument = {"-private","-sourcepath", "./exemples","-doclet" ,"pumlFromJava.FirstDoclet","-docletpath" ,"","western" } ;
         ToolProvider toolProvider = ToolProvider.findFirst("javadoc").get();
         System.out.println(toolProvider.name());
 
@@ -14,6 +16,6 @@ public class Java2Puml
     javadoc -private -sourcepath <src> -doclet pumlFromJava.FirstDoclet -docletpath out/production/<projet>
       <package> ... <fichiers>
  */
-        toolProvider.run(System.out, System.err, args);
+        toolProvider.run(System.out, System.err, argument);
     }
 }
