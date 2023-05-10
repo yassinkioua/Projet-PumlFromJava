7d6d264ab0fb7d8bcd64c191cf63cad7572f6c03
xeragon <wladimir.bozenko@etu.unistra.fr>
2023-05-10 16:16:41 +0200

    tfoulik

--- src/pumlFromJava/PumlDiagram.java
+++ src/pumlFromJava/PumlDiagram.java
@@ -7,11 +7,12 @@ import jdk.javadoc.doclet.Reporter;
 
 import java.io.File;
 import java.io.FileWriter;
-
+import java.io.IOException;
 
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -42,7 +43,8 @@ public class PumlDiagram{
                     
                     for (Element e : temp) {
                         // System.out.println(e);
-                        fw.write(e.toString() + "\n");
+                        // fw.write(e.toString() + "\n");
+                        attributeWrite(fw,e);
                         // System.out.println(e.asType());
                         // System.out.println(e.getSimpleName()); donne le nom de la methode sans parenthese et sans les param
                         // System.out.println(e.getModifiers()); donne les info du genre public ou private et final 
@@ -58,6 +60,50 @@ public class PumlDiagram{
             
         } catch (Exception e) {
             // TODO: handle exception
+        }
+    }
+
+    private static void attributeWrite(FileWriter fw, Element e){
+        try {
+            if(e.getKind() == ElementKind.FIELD){
+                for (Modifier mod : e.getModifiers()) {
+                    System.out.println(mod.toString());
+                    
+                    if(mod == Modifier.PRIVATE){
+                        fw.write("- " );
+                    }
+                    if(mod == Modifier.PUBLIC){
+                        fw.write("+ ");
+                    }
+
+                    if(mod == Modifier.STATIC){
+                        fw.write("{static} ");
+                    }
+
+                    if(mod == Modifier.FINAL){
+                        fw.write("final ");
+                    }
+                    if(mod == Modifier.ABSTRACT){
+                        fw.write("{abstract} ");
+                    }
+                    if(mod == Modifier.PROTECTED){
+                        fw.write("# ");
+                    }
+                    // fw.write(mod.toString() + " ");
+
+                }
+                fw.write(e.toString() + "\n");
+            }
+
+            // fw.write("\n");
+            // System.out.println(e.asType().toString());
+            // System.out.println(e.getKind());
+            // System.out.println(e.getModifiers());
+            
+            
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
         }
     }
     
