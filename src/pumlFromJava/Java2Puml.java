707ac5956ac50ebdfe00f79fbd880070afe41c67
Yassin Kioua <yassin.kioua@etu.unistra.fr>
2023-06-10 00:06:04 +0200

    Dernière maj des rapports et des diagrammes

--- src/pumlFromJava/Java2Puml.java
+++ src/pumlFromJava/Java2Puml.java
@@ -10,13 +10,13 @@ public class Java2Puml
     private static String umlType;
     public static void main(String[] args)
     {   
-        // String umlType = "DCA";
+        //String umlType = "DCA";
         String umlType = "DCC";
 
        
-        // String[] argument = {"-private","-sourcepath", "./exemples","-doclet" ,"pumlFromJava.PumlDoclet","-d","./uml","-out","westernDCA.puml","-umlType",umlType,"-docletpath" ,"","western" } ;
-        String[] argument = {"-private","-sourcepath", "./","-doclet" ,"pumlFromJava.PumlDoclet","-d","./uml","-out","testDCA.puml","-umlType",umlType,"-docletpath" ,"","test" } ;
-        //String[] argument = {"-private","-sourcepath", "./src","-doclet" ,"pumlFromJava.PumlDoclet","-d","./uml","-docletpath" ,"","pumlFromJava" } ;
+        //String[] argument = {"-private","-sourcepath", "./exemples","-doclet" ,"pumlFromJava.PumlDoclet","-d","./uml","-out","westernDCA.puml","-umlType",umlType,"-docletpath" ,"","western" } ;
+        //String[] argument = {"-private","-sourcepath", "./","-doclet" ,"pumlFromJava.PumlDoclet","-d","./uml","-out","testDCA.puml","-umlType",umlType,"-docletpath" ,"","test" } ;
+        String[] argument = {"-private","-sourcepath", "./src","-doclet" ,"pumlFromJava.PumlDoclet","-d","./uml","-out","DCC_pumlFromJava.puml","-umlType",umlType,"-docletpath" ,"","pumlFromJava" } ;
         ToolProvider toolProvider = ToolProvider.findFirst("javadoc").get();
         System.out.println(toolProvider.name());
         toolProvider.run(System.out, System.err, argument);
